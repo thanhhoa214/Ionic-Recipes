8c6c930e68e9327d57a5bda0b87a0017ccc1e9e9
thanhhoa214 <thanhthanhhoa123@gmail.com>
2020-02-19 18:43:05 +0700

    Add some pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    path: "",
+    loadChildren: () => import("./home/home.module").then(m => m.HomePageModule)
+  },
+  {
+    path: "home",
+    loadChildren: () => import("./home/home.module").then(m => m.HomePageModule)
+  },
+  {
+    path: "my-order",
+    loadChildren: () =>
+      import("./order/order.module").then(m => m.OrderPageModule)
+  },
+  {
+    path: "saved",
+    loadChildren: () =>
+      import("./saved/saved.module").then(m => m.SavedPageModule)
+  },
+  {
+    path: "me",
+    loadChildren: () =>
+      import("./profile/profile.module").then(m => m.ProfilePageModule)
+  },
+  {
+    path: 'foods/:id',
+    loadChildren: () => import('./food-detail/food-detail.module').then( m => m.FoodDetailPageModule)
   }
 ];
 @NgModule({
